1cf2de90eecb42128382b9a317f666b8364c0213
markdmy <markdmy@hotmail.com>
2023-07-18 17:09:16 -0400

    fixed physics for tilemap and player

--- scene1.js
+++ scene1.js
@@ -13,22 +13,21 @@ class scene1 extends Phaser.Scene {
     this.cameras.main.setBounds(0, 0, 3392, 100);
     this.physics.world.setBounds(0, 0, 3000, 3000);
     this.physics.world.setBoundsCollision(true, true, true, true);
-    
-    const tiles = this.map.addTilesetImage('ground_tiles', 'tiles');
+    this.cursors = this.input.keyboard.createCursorKeys();
 
- 
+    const tiles = this.map.addTilesetImage('ground_tiles', 'tiles');
     this.floor = this.map.createStaticLayer('ground', tiles, 0, 0);
     this.walls = this.map.createStaticLayer('walls', tiles, 0, 0);
     this.trees = this.map.createStaticLayer('trees', tiles, 0, 0);
     this.stairs = this.map.createStaticLayer('bridge_stairs', tiles, 0, 0);
-    this.walls.setCollisionByProperty({ collides: true });
 
-    this.cursors = this.input.keyboard.createCursorKeys();
-    this.enemy = this.physics.add.sprite(250, 250, 'enemy').setScale(0.15);
-    this.player = this.physics.add.sprite(200, 200, 'player');
+    this.enemy = this.physics.add.sprite(250, 250, 'enemy').setScale(0.13);
+    this.player = this.physics.add.sprite(150, 150, 'player');
 
-    this.physics.add.collider(this.player, this.walls);
-    this.physics.add.collider(this.player, this.enemy);
+    //physics
+    this.floor.setCollisionByProperty({ collides: true });
+    this.physics.add.collider(this.player, this.floor, this.hit, null, this);
+    this.physics.add.collider(this.player, this.enemy, this.hit, null, this);
 
     // Create animations
     this.anims.create({
@@ -44,28 +43,37 @@ class scene1 extends Phaser.Scene {
       frameRate: 10,
       repeat: -1
     });
+
     this.cameras.main.setBounds(0, 0, 3392, 1900);
     this.cameras.main.startFollow(this.player, true, 0.08, 0.08);
+
+    console.log(this.walls)
   }
 
+  hit() {
+    console.log('hi')
+  }
 
   update() {
-    let speed = 1;
-    if (this.cursors.up.isDown) {
-      this.player.y -= speed;
-    }
+    // Set the player's velocity to control movement
+    const speed = 100;
 
-    if (this.cursors.down.isDown) {
-      this.player.y += speed;
+    this.player.setVelocity(0, 0); // Reset the velocity on each update
+
+    if (this.cursors.up.isDown) {
+      this.player.setVelocityY(-speed);
+    } else if (this.cursors.down.isDown) {
+      this.player.setVelocityY(speed);
     }
 
     if (this.cursors.left.isDown) {
-      this.player.x -= speed;
+      this.player.setVelocityX(-speed);
       this.player.anims.play('moveLeft', true);
     } else if (this.cursors.right.isDown) {
-      this.player.x += speed;
+      this.player.setVelocityX(speed);
       this.player.anims.play('moveRight', true);
     } else {
+      this.player.setVelocityX(0);
       this.player.anims.stop(); // Stop the animation when no arrow key is pressed
     }
   }
